4361f844ab08ebab9f5c9b5e702baf515820e6b1
amatranga <amatranga892@gmail.com>
2017-12-20 00:09:13 -0600

    Implement server controllers

---
+++ server/controllers/users.js
@@ -0,0 +1,20 @@
+const models = require('../../db/models');
+
+module.exports.update = (req, res) => {
+  models.User.where({ email: req.params.email }).fetch()
+    .then(profile => {
+      if (!profile) {
+        throw profile;
+      }
+      return profile.save(req.body, { method: 'update' });
+    })
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .error(err => {
+      res.status(500).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+};
