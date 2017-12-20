43859c6ca27bca5f03da853e97f7cf98a2ef15e3
amatranga <amatranga892@gmail.com>
2017-12-19 22:36:20 -0600

    Implements initial migration file

---
+++ db/migrations/20171219171327_initial.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('users', function(table) {
+      table.increments('id').unsigned().primary();
+      table.string('username', 100).nullable();
+      table.string('email', 100).nullable().unique();
+      table.string('first_name', 100).nullable();
+      table.string('last_name', 100).nullable();
+      table.string('phone_number', 20).nullable();
+      table.string('street', 100).nullable();
+      table.string('city', 100).nullable();
+      table.string('zip_code', 5).nullable();
+    }),
+    knex.schema.createTableIfNotExists('auths', function(table) {
+      table.increments('id').unsigned().primary();
+      table.string('password', 100).nullable();
+      table.string('salt', 100).nullable();
+      table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('auths'),
+    knex.schema.dropTable('users')
+  ]);
+};
